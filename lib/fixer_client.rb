67433b6e0606d55f3f2b8e480f89fb58419f4521
Collins Lagat <cklagat@gmail.com>
2022-02-17 14:33:12 +0300

    update client to use new api

--- lib/fixer_client.rb
+++ lib/fixer_client.rb
@@ -2,21 +2,25 @@ class FixerClient
   attr_reader :uri
 
   def initialize(base, symbols)
-    symbols_formatted = [*symbols].select { |s| s.is_a? String }.map(&:upcase).join(',')
+    base_formatted = base.upcase
+    symbols_formatted = [*symbols].select { |s| s.is_a? String }.map(&:upcase).map { |c| "#{base_formatted}_#{c}" }.join(',')
     raise ArgumentError, 'Symbols are blank' if symbols_formatted.blank?
     raise ArgumentError, 'Base currency is blank' if base.blank?
-    base_formatted = base.upcase
+
+    fx_conv_api_key = ENV['CURRENCY_CONVERTER_API']
+
     @uri = URI(
-      "http://api.fixer.io/latest?base=#{base_formatted}&symbols=#{symbols_formatted}"
+      "https://free.currconv.com/api/v7/convert?apiKey=#{fx_conv_api_key}&q=#{symbols_formatted}"
     )
   end
 
   def fetch
     response = Net::HTTP.get(uri)
-    parsed_response = JSON.parse(response)
-    return if parsed_response['rates'].blank?
-    rates = parsed_response['rates']
-    rates.count == 1 ? rates.first[1] : rates
+    parsed_response = JSON.parse response, symbolize_names: true
+
+    return if parsed_response[:results].blank?
+
+    parsed_response[:results].values
   rescue StandardError => _e
     nil
   end
