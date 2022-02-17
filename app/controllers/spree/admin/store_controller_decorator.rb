452bafeb960edc867a044076a8af99ed1d806a53
Collins Lagat <cklagat@gmail.com>
2022-02-17 14:28:22 +0300

    use stores controller insteal of general settings

---
+++ app/controllers/spree/admin/store_controller_decorator.rb
@@ -0,0 +1,16 @@
+module Spree
+  module Admin
+    module StoresControllerDecorator
+      def self.prepended(base)
+        base.after_action :create_fx_rates_for_currencies, only: %i[create update]
+      end
+
+      def create_fx_rates_for_currencies
+        # Spree::FxRate.create_supported_currencies((current_store)) if Spree::FxRate.none? || @store.supported_currencies_previously_changed?
+        Spree::FxRate.create_supported_currencies(current_store)
+      end
+    end
+  end
+end
+
+Spree::Admin::StoresController.prepend(Spree::Admin::StoresControllerDecorator)
