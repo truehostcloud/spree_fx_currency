d306aa117d49031f5c9a990467f6ee15bc78d6fe
Collins Lagat <cklagat@gmail.com>
2022-02-17 16:29:01 +0300

    when updating products and variants, only use supported currencies

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,7 +5,15 @@ module Spree
     end
 
     def update_prices
-      Spree::FxRate.all.find_each { |r| r.update_prices_for_variant(self) }
+      product.stores.each do |store|
+        default_currency = store.default_currency
+        supported_currencies = store.supported_currencies.split(',')
+                                    .reject { |c| default_currency.to_s == c.upcase }
+        supported_currencies.map do |c|
+          fx = Spree::FxRate.where(from_currency: default_currency, to_currency: c.upcase).first
+          fx.update_prices_for_variant(self)
+        end
+      end
     end
   end
 end
