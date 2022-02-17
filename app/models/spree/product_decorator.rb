6d2e43b8277c09c28d461baaf27b5378d9cca39c
Collins Lagat <cklagat@gmail.com>
2022-02-17 15:59:36 +0300

    fix: update to fix spree/product_decorator.rb to define constant Spree::ProductDecorator, but didn't

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,13 @@
-Spree::Product.class_eval do
-  after_save :update_prices
+module Spree
+  module ProductDecorator
+    def self.prepended(base)
+      base.after_save :update_prices
+    end
 
-  def update_prices
-    Spree::FxRate.all.each { |r| r.update_prices_for_product(self) }
+    def update_prices
+      Spree::FxRate.all.find_each { |r| r.update_prices_for_product(self) }
+    end
   end
 end
+
+Spree::Product.prepend(Spree::ProductDecorator)
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,7 +1,13 @@
-Spree::Variant.class_eval do
-  after_save :update_prices
+module Spree
+  module VariantDecorator
+    def self.prepended(base)
+      base.after_save :update_prices
+    end
 
-  def update_prices
-    Spree::FxRate.all.each { |r| r.update_prices_for_variant(self) }
+    def update_prices
+      Spree::FxRate.all.find_each { |r| r.update_prices_for_variant(self) }
+    end
   end
 end
+
+Spree::Variant.prepend(Spree::VariantDecorator)
