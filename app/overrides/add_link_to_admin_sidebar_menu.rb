c55753b01ceb8fb34065c201a73d375a2e009cb6
willymwai <willymwaid92@gmail.com>
2024-12-05 13:12:42 +0300

    chore: disable adding to sidemenu

--- app/overrides/add_link_to_admin_sidebar_menu.rb
+++
@@ -1,8 +0,0 @@
-module AddLinkToAdminSidebarMenu
-  Deface::Override.new(
-    virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-    name: 'fx_rate_admin_sidebar_menu',
-    insert_bottom: '#sidebar-configuration',
-    partial: 'spree/admin/shared/fx_rate_sidebar_menu'
-  )
-end
