532c1295a6cff83aaffcccac1ad7a9ec014899e4
willymwai <willymwaid92@gmail.com>
2024-12-05 11:13:29 +0300

    fix: wrap Deface override in module to avoid constant redefinition

--- app/overrides/add_link_to_admin_sidebar_menu.rb
+++ app/overrides/add_link_to_admin_sidebar_menu.rb
@@ -1,6 +1,8 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-  name: 'fx_rate_admin_sidebar_menu',
-  insert_bottom: '#sidebar-configuration',
-  partial: 'spree/admin/shared/fx_rate_sidebar_menu'
-)
+module AddLinkToAdminSidebarMenu
+  Deface::Override.new(
+    virtual_path: 'spree/admin/shared/sub_menu/_configuration',
+    name: 'fx_rate_admin_sidebar_menu',
+    insert_bottom: '#sidebar-configuration',
+    partial: 'spree/admin/shared/fx_rate_sidebar_menu'
+  )
+end
